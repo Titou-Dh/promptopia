b027d82acc715d43a4591b7d707a8fb6f10fdaa4
Titou-Dh <titoudh1@gmail.com>
2024-03-26 03:59:40 +0100

    completeed  the api for fetch all posts for specefic id

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,13 +1,25 @@
 "use client";
 
-import { useState ,useEffect } from 'react';
-import {useSession} from 'next-auth/react';
-import {useRouter} from 'next/navigation';
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 import Profile from '@components/Profile';
 
 
 const MyProfile = () => {
+    const { data: session } = useSession();
+    const [posts, setPosts] = useState([]);
+
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch(`/api/users/${session.user.id}/posts`);
+            const data = await response.json()
+            setPosts(data);
+        }
+        if (session?.user.id) fetchPosts();
+    }, [])
     const handleEdit = () => {
 
     }
@@ -22,7 +34,7 @@ const MyProfile = () => {
             <Profile
                 name="My"
                 desc="welcome to yout personalized prifile page"
-                data={[]}
+                data={posts}
                 handleEdit={handleEdit}
                 handleDelete={handleDelete}
 
